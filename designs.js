74974274296033d96a020c88e838a6dc258a5fea
Лилия Шаргородская <liliasargorodskaa@Lilias-MacBook-Pro.local>
2021-12-22 18:03:11 +0200

    add comment

--- designs.js
+++ designs.js
@@ -2,24 +2,32 @@
 const table = document.body.querySelector('#pixelCanvas');
 const myCanvas = document.body.querySelector('#sizePicker');
 
-myCanvas.addEventListener('submit',function(event){
-  event.preventDefault();//To prevent reloading the page, when click submit
-  makeGrid();
+myCanvas.addEventListener('submit',function(event)
+    {
+    event.preventDefault();//To prevent reloading the page, when click submit
+    makeGrid();
 });
-
+/*
+  * function create a grid of squares
+  * currentWidth- access to the number of squares representing the width of the grid
+  *  currentHeight- access to the number of squares representing the height of the grid
+*/
 function makeGrid() {
     table.innerHTML = ''; //Sets the HTML content inside table;
     var currentHeight= document.body.querySelector('#inputHeight').value;
     var currentWidth = document.body.querySelector('#inputWidth').value;
     for(row = 0;row < currentHeight; row++){
-        var newRow = table.insertRow()
+        var newRow = table.insertRow();
         for(column = 0; column < currentWidth; column++)//Nested loop
         newRow.insertCell();
     }
 
 
-};
-//You can color the pixels in the grid
+}
+/*
+*You can color the pixels in the grid
+*Event listeners are properly added to the grid squares
+*/
 table.addEventListener('click',function(event){
     const currentColor = document.body.querySelector('#colorPicker').value;
     event.target.style.backgroundColor = currentColor;
